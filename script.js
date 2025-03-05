f51e09f0a6cf4da9806b1da73f842108cad8054c
Raff_12 <raffelecoppola381@gmail.com>
2025-03-05 16:56:08 +0100

    feat: Iniziazione del progetto

---
+++ script.js
@@ -0,0 +1,18 @@
+//FASI DEL PROGETTO:
+//Scrivere un programma che chieda all’utente:
+// - Il numero di chilometri da percorrere
+// - Età del passeggero
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
+
+//Nominazione delle seguenti variabili
+let Inp_name_user = document.getElementById('input-name');
+let Inp_age_user = document.getElementById('input-name');
+let Inp_select_gender = document.getElementById('input-name');
+let Inp_btn_gen = document.getElementById('button-sub');
+let Inp_btn_reset = document.getElementById('reset-btn');
+const Form_Inp_sc = document.getElementById('form-inp');
+
+//Creazione sul relativo procedimento di calcolo del biglietto
