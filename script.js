343578b2ef7521a334d351574b8e1b568e5df939
Raff_12 <raffelecoppola381@gmail.com>
2025-03-05 17:46:33 +0100

    feat: Pubblicazione delle seguenti risorse

--- script.js
+++ script.js
@@ -9,10 +9,49 @@
 
 //Nominazione delle seguenti variabili
 let Inp_name_user = document.getElementById('input-name');
-let Inp_age_user = document.getElementById('input-name');
-let Inp_select_gender = document.getElementById('input-name');
-let Inp_btn_gen = document.getElementById('button-sub');
-let Inp_btn_reset = document.getElementById('reset-btn');
+let Inp_km_user = document.getElementById('input-km');
+let Inp_select_gender = document.getElementById('input-select-gender');
+const Inp_btn_gen = document.getElementById('button-sub');
+const Inp_btn_reset = document.getElementById('reset-btn');
 const Form_Inp_sc = document.getElementById('form-inp');
 
 //Creazione sul relativo procedimento di calcolo del biglietto
+
+function HandleClickForm(e){
+    e.preventDefault();
+    let base_km_bg = 0.21;
+    let value_name = Inp_name_user.value;
+    let value_km = Number(Inp_km_user.value);
+    let value_select = Inp_select_gender.value;
+    let calc_km_op = 0;
+    let calc_perc = 0;
+    if (value_name !== '' && value_km !== 0 && value_select !== '') {
+        if (value_select === 'Minorenne') {
+            calc_km_op = value_km * base_km_bg;
+            calc_perc = (calc_km_op * 20) / 100;
+            calc_perc.toFixed(2);
+        } else if(value_select === 'Maggiorenne'){
+            calc_km_op = value_km * base_km_bg;
+            calc_perc = (calc_km_op * 20) / 100;
+            calc_perc.toFixed(2);
+        } else if (value_select === 'Adulto'){
+            calc_km_op = value_km * base_km_bg;
+            calc_perc = (calc_km_op * 40) / 100;
+            calc_perc.toFixed(2);
+        }
+        console.log(`Biglietto è di ${value_name} è ${value_select} il suo costo principale è di $${calc_perc}`);
+    } else {
+        window.alert('Valori negli input corrispondenti');
+    }
+}
+
+//Creazione di funzione del reset in questione
+
+function HandleClickReset(e){
+    e.preventDefault();
+    Form_Inp_sc.reset();
+}
+
+Form_Inp_sc.addEventListener('submit', (e)=>{ e.preventDefault()});
+Inp_btn_gen.addEventListener('click', HandleClickForm);
+Inp_btn_reset.addEventListener('click', HandleClickReset);
